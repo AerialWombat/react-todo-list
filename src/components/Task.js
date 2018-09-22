1049c9fa1e4ec857b0853b3cc7c9348aa7b29d13
AerialWombat <adrianzhan@gmail.com>
2018-09-21 22:16:30 -0400

    Adding material icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Task.css';
 
 const Task = ({ reactKey, task, taskClick }) => {
     return (
         <div data-key={reactKey} className='mt3 mb3 pa2 pv1 mw7 flex flex-row justify-start grow shadow-hover' style={{ 'overflow-wrap': 'break-word', cursor: 'default' }}>
-            <input data-key={reactKey} className='mr2 fl pointer' type='button'  value='Done' onClick={ taskClick }/>
+            <button data-key={reactKey} className='mr2 fl pointer material-icons' type='button' onClick={ taskClick }>
+                <i>play_arrow</i>
+            </button>
             <label className='' for={task}>{task}</label>
+            <i className='material-icons'>face</i>
         </div>
     )
 }
