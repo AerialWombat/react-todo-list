86d10c89daa36fdc13cc82cdf8ecc324a5915ff2
AerialWombat <adrianzhan@gmail.com>
2018-09-24 19:25:52 -0400

    Added material icons and Montserrat font.

--- src/components/AddBar.js
+++ src/components/AddBar.js
@@ -3,16 +3,13 @@ import React from 'react';
 const AddBar = ({ addSubmit }) => {
     return (
         <form className='' onSubmit={addSubmit}>
-            <input
-                className=''
-                type='submit'
-                value='Add'
-            />
+            <button className='pa2 b--none pointer dim material-icons' style={{background: 'none'}} type='submit' value='Add'>add_box</button>
             <input
                 className='mr2 bb b--solid b--silver bw1 bt-0 br-0 bl-0'
                 style={{ background: 'transparent' }}
                 type='text'
                 name='task'
+                autoComplete='off'
                 placeholder='Enter new task'
             />
         </form>
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import './Task.css';
 
 const Task = ({ reactKey, task, taskClick }) => {
     return (
-        <div data-key={reactKey} className='mt3 mb3 pa2 pv1 mw7 flex flex-row justify-start grow shadow-hover' style={{ 'overflow-wrap': 'break-word', cursor: 'default' }}>
-            <button data-key={reactKey} className='mr2 fl pointer material-icons' type='button' onClick={ taskClick }>
-                <i>play_arrow</i>
+        <div data-key={reactKey} className='mt3 mb3 pa2 pv1 mw7 bb b--silver flex flex-row justify-start shadow-hover' style={{ 'overflow-wrap': 'break-word', cursor: 'default' }}>
+            <button data-key={reactKey} className='mr2 b--none pointer dim material-icons' style={{background: 'none'}} type='button' onClick={ taskClick }>
+                <i className='' >check_circle</i>
             </button>
             <label className='' for={task}>{task}</label>
-            <i className='material-icons'>face</i>
         </div>
     )
 }
