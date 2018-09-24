bf1155add474b3bada30beb907492112d21bb422
AerialWombat <adrianzhan@gmail.com>
2018-09-24 19:36:04 -0400

    Added min-width to text input. Aligned buttons and text

--- src/components/AddBar.js
+++ src/components/AddBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AddBar = ({ addSubmit }) => {
     return (
-        <form className='' onSubmit={addSubmit}>
+        <form className='flex flex-row items-center' style={{minWidth: '300px'}} onSubmit={addSubmit}>
             <button className='pa2 b--none pointer dim material-icons' style={{background: 'none'}} type='submit' value='Add'>add_box</button>
             <input
                 className='mr2 bb b--solid b--silver bw1 bt-0 br-0 bl-0'
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Task = ({ reactKey, task, taskClick }) => {
     return (
-        <div data-key={reactKey} className='mt3 mb3 pa2 pv1 mw7 bb b--silver flex flex-row justify-start shadow-hover' style={{ 'overflow-wrap': 'break-word', cursor: 'default' }}>
+        <div data-key={reactKey} className='mt3 mb3 pa2 pv1 mw7 bb b--silver flex flex-row justify-start items-center shadow-hover' style={{ 'overflow-wrap': 'break-word', cursor: 'default' }}>
             <button data-key={reactKey} className='mr2 b--none pointer dim material-icons' style={{background: 'none'}} type='button' onClick={ taskClick }>
                 <i className='' >check_circle</i>
             </button>
